2a0ccc7f9bfea21a2a2e1b3a4ce4ac3eafb69d4e
Ali Ahadi <ali.ahadi.official@gmail.com>
2025-04-08 18:02:34 +0330

    generate dummy data

---
+++ core/shop/management/commands/dummy_data.py
@@ -0,0 +1,70 @@
+from django.core.management.base import BaseCommand
+from django.utils.text import slugify
+from faker import Faker
+import random
+from decimal import Decimal
+
+from shop.models import Product, ProductCategory
+from django.contrib.auth import get_user_model
+from shop.models import ProductStatus
+
+fake = Faker()
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = "Seed the database with 10 fake products and 10 categories"
+
+    def handle(self, *args, **kwargs):
+        users = list(User.objects.all())
+        if not users:
+            self.stdout.write(
+                self.style.ERROR("❌ No users found. Please create a user first.")
+            )
+            return
+
+        self.stdout.write("📦 Creating 10 categories...")
+        categories = self.create_categories()
+
+        self.stdout.write("🛒 Creating 10 products...")
+        self.create_products(users, categories)
+
+        self.stdout.write(self.style.SUCCESS("✅ Seeding completed successfully!"))
+
+    def create_categories(self):
+        categories = []
+        for _ in range(10):
+            title = fake.unique.word().capitalize()
+            slug = slugify(title, allow_unicode=True)
+            category = ProductCategory.objects.create(title=title, slug=slug)
+            categories.append(category)
+        return categories
+
+    def create_products(self, users, categories):
+
+        for _ in range(10):
+            user = random.choice(users)
+            title = fake.sentence(nb_words=3).rstrip(".")
+            slug = slugify(title)
+            description = fake.paragraph(nb_sentences=3)
+            stock = random.randint(1, 100)
+            status = random.choice([status.value for status in ProductStatus])
+            price = Decimal(random.uniform(10.0, 1000.0)).quantize(Decimal("0.01"))
+            discount = random.randint(0, 50)
+
+            product = Product.objects.create(
+                user=user,
+                title=title,
+                slug=slug,
+                description=description,
+                stock=stock,
+                status=status,
+                price=price,
+                discount_percnete=discount,
+            )
+
+            selected_categories = random.sample(
+                categories, k=random.randint(1, len(categories))
+            )
+            product.category.set(selected_categories)
+            product.save()
