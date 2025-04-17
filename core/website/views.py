b22178edc673a4643c19137dfd9f2ee50999b1a7
Ali Ahadi <ali.ahadi.official@gmail.com>
2025-04-17 15:40:00 +0330

    fix form bug

--- core/website/views.py
+++ core/website/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView,CreateView
 from .forms import NewsLetterForm
 from .models import Newsletter
-
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -15,6 +16,13 @@ class SubscribeView(CreateView):
     model=Newsletter
     form_class=NewsLetterForm
     success_url='/'
+    def post(self, request, *args, **kwargs):
+        form=self.form_class(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else :
+           return HttpResponse('email not valid')
 
 
     
