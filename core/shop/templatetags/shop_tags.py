5d5f348154e760da8323e35025c751edc11ed372
Ali Ahadi <ali.ahadi.official@gmail.com>
2025-04-17 14:04:36 +0330

    add similar product

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -8,3 +8,10 @@ def show_leatest_products():
     leatest_products=Product.objects.filter(status=ProductStatus.publish.value).order_by('-created_at')[:8]
     
     return {'leatest_products':leatest_products}
+
+@register.inclusion_tag('includes/similar_products.html')
+def show_similar_products(product):
+    product_categories= product.category.all()
+    similar_products=Product.objects.filter(status=ProductStatus.publish.value,category__in=product_categories)[:4]
+    
+    return {'similar_products':similar_products}
