612ad8cdb75c5bc1416fe684a07f150f7d3031f1
Ali Ahadi <ali.ahadi.official@gmail.com>
2025-05-14 18:50:25 +0330

    add password edit in dashboard

--- core/dashboard/admin/urls.py
+++ core/dashboard/admin/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from dashboard.admin.views import(AdminDashboardHomeView)
+from .views import AdminSecurityView
 
 app_name='admin'
 
 urlpatterns=[
-    path('home/',AdminDashboardHomeView.as_view(),name='home')
+    path('home/',AdminDashboardHomeView.as_view(),name='home'),
+    path('security-edit/',AdminSecurityView.as_view(),name='security-edit')
 ]
--- core/dashboard/admin/views.py
+++ core/dashboard/admin/views.py
@@ -1,7 +1,17 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from dashboard.permissions import IsAdmin
+from django.contrib.auth.views import PasswordChangeView as auth_view
+from .forms import AdminPasswordChangeForm
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 
 
 class AdminDashboardHomeView(LoginRequiredMixin,IsAdmin,TemplateView):
     template_name='dashboard/admin/home.html'
+
+class AdminSecurityView(LoginRequiredMixin,IsAdmin,TemplateView,SuccessMessageMixin,auth_view,):  
+    form_class=AdminPasswordChangeForm
+    template_name='dashboard/admin/account-security.html'
+    success_message='رمز عبور با موفقیت بروزرسانی شد!'
+    success_url=reverse_lazy('dashboard:admin:security-edit')
