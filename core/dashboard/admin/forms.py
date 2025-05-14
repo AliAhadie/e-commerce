b09ea03406e64fcdf3b31e7f709a33f3746471c5
Ali Ahadi <ali.ahadi.official@gmail.com>
2025-05-14 19:32:37 +0330

    add error persian error messages

--- core/dashboard/admin/forms.py
+++ core/dashboard/admin/forms.py
@@ -1,4 +1,18 @@
 from django.contrib.auth.forms import PasswordChangeForm
+from django.utils.translation import gettext_lazy as _
 
 class AdminPasswordChangeForm(PasswordChangeForm):
-    pass
+        error_messages = {
+       
+        "password_incorrect": _(
+            "رمزعبور فعلی شما اشتباه است !"
+        ),
+        "password_mismatch": _(" رمز عبور های جدید شما باهم تطابق ندارند!"),
+
+        }
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            self.fields['old_password'].error_messages['required'] = _("وارد کردن رمز عبور فعلی الزامی است!")
+            self.fields['new_password1'].error_messages['required'] = _("لطفاً رمز عبور جدید را وارد کنید!")
+            self.fields['new_password2'].error_messages['required'] = _("تکرار رمز عبور جدید الزامی است!")
