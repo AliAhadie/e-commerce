96e4b0106349d3b695c08cc5c9df4db52bb083a7
Ali Ahadi <ali.ahadi.official@gmail.com>
2025-07-12 07:34:53 +0330

    shit

--- core/core/settings.py
+++ core/core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]# Application definition
 
