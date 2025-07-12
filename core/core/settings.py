c2c345e7004768d192ccd72769ab607293fff9a9
Ali Ahadi <ali.ahadi.official@gmail.com>
2025-07-12 06:15:10 +0330

    deploy start

--- core/core/settings.py
+++ core/core/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,7 +139,8 @@ MEDIA_URL='media/'
 
 STATIC_ROOT=BASE_DIR/'staticfiles'
 MEDIA_ROOT= BASE_DIR/'media'
-STATICFILES_DIRS= [BASE_DIR/'static']
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
